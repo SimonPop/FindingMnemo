4bc7f7a24d27b9cf572410ba17b418fd9e81f5c7
simon <simon.popelier@gmail.com>
2022-08-08 00:16:05 +0200

    Basic infra classes

---
+++ src/PhoneticEmbedder.py
@@ -0,0 +1,17 @@
+from dragonmapper import transcriptions
+import eng_to_ipa as ipa
+import panphon.distance
+
+
+class PhoneticEmbedder:
+    def __init__(self):
+        self.dst = panphon.distance.Distance()
+
+    def encode_pinyin(self, string: str) -> str:
+        return transcriptions.pinyin_to_ipa(string)
+
+    def encode_english(self, string: str) -> str:
+        return ipa.convert(string)
+
+    def naive_distance(self, str1: str, str2: str) -> float:
+        return self.dst.levenshtein_distance(str1, str2)

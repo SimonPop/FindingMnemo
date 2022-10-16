7039c92845a6c388efc0c6e554392dadfd235888
simon <simon.popelier@gmail.com>
2022-10-16 20:54:23 +0200

    Moved legacy files away

---
+++ src/legacy/corpus_creator.py
@@ -0,0 +1,15 @@
+import wikipediaapi
+import re
+
+class CorpusCreator():
+    def __init__(self):
+        self.wiki_wiki = wikipediaapi.Wikipedia('en')
+        
+
+    def create_corpus(self, page_name: str):
+        page = self.wiki_wiki.page(page_name)
+        text = page.text
+        text = text.replace('\n', '')
+        text = text.replace('\'', '')
+        with open("corpus_{}.txt".format(page_name), "w", encoding="utf-8") as text_file:
+            text_file.write(u'{}'.format(text))

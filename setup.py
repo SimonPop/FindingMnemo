a0fe27b1fbd528281f78e2650dcc2148a919617c
simon <simon.popelier@gmail.com>
2022-10-16 20:54:56 +0200

    Setup files

---
+++ setup.py
@@ -0,0 +1,16 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "finding_mnemo",
+    version = "0.0.1",
+    author = "Simon Popelier",
+    author_email = "simon.popelier@gmail.com",
+    description = ("Finding mnemomnichs."),
+    license = "BSD",
+    keywords = "example documentation tutorial",
+    packages=['src'],
+)

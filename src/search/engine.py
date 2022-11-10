5e25006ae84120cad02bd39a370e2afce2025e3c
Simon Popelier <simon.popelier@gmail.com>
2022-11-10 23:10:04 +0100

    Jina Executor refacto for Engine

--- src/search/engine.py
+++ src/search/engine.py
@@ -1,22 +1,24 @@
-from jina import Document, DocumentArray, Executor
-from pathlib import Path
-import pandas as pd 
-import torch
 from src.model.sound_siamese_v2 import SoundSiamese
 
-class Engine:
-    # TODO: use executor
+from jina import requests, DocumentArray, Document, Executor, Flow
+from typing import Dict, Union
+from pathlib import Path
+import torch
+
+class Engine(Executor):
     n_limit: int = 5
     model: SoundSiamese
     da: DocumentArray
 
-    def __init__(self):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
         self.model = self.load_model()
         self.da = self.documents()
 
-    def find(self, word: str):
-        np_query = self.model.encode([word]).detach().numpy()[0]
-        print(np_query)
+    @requests(on=['/search'])
+    def search(self, docs: DocumentArray, **kwargs) -> Union[DocumentArray, Dict, None]:
+        x = docs[:,'tags__ipa']
+        np_query = self.model.encode(x).detach().numpy()[0]
         return self.da.find(np_query, limit=self.n_limit)
 
     def load_model(self) -> SoundSiamese:
@@ -34,3 +36,16 @@ class Engine:
                 'distance': 'COSINE'
             },
         )
+
+
+# f = Flow().add(uses=Engine)
+
+# with f:
+#     f.post(on='/search', inputs=DocumentArray(Document(text='hotel', ipa='bɔ́təl')))
+
+
+# m = Engine()
+# da = DocumentArray([Document(text='hotel', ipa='bɔ́təl')])
+# res = m.search(da)
+# print(res[:,'text'])
+# print(res.summary())

009f5eeb224a187aafb9fb0d78f8ea20a08205fd
Simon Popelier <simon.popelier@gmail.com>
2022-11-12 00:05:06 +0100

    Jina App creates a flow containing 3 executors

---
+++ src/jina-app/flow.py
@@ -0,0 +1,11 @@
+from jina import Flow, Document, DocumentArray
+from src.generation.text_generator import TextGenerator
+from src.search.engine import Engine
+from src.search.indexer import Indexer
+
+
+if __name__ == "__main__":
+    f = Flow().add(name='Indexer', uses=Indexer).add(name='Engine', uses=Engine, needs=[]).add(name='TextGenerator', uses=TextGenerator, needs=['Engine'])
+
+    with f:
+        f.post(on='/generate', inputs=DocumentArray(Document(text='hotel', ipa='bɔ́təl')), on_done=print)

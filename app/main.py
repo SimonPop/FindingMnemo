46ecf8f3a632b2be5670d53dd8b973e669afe5e7
Simon Popelier <simon.popelier@gmail.com>
2022-11-20 18:37:31 +0100

    FastAPI app for API access to search, index and generating

---
+++ app/main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+from jina import DocumentArray, Document
+
+from src.generation.text_generator import TextGenerator
+from src.search.engine import Engine
+from src.search.indexer import Indexer
+
+from src.utils.ipa import convert_mandarin_to_ipa
+
+from typing import List
+
+app = FastAPI()
+
+# Executors:
+indexer = Indexer()
+generator = TextGenerator()
+engine = Engine()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/index/")
+def index_items():
+    indexer.index()
+    engine.load_documents()
+
+@app.get("/search/{word}/")
+def search(word: str):
+    ipa: str = convert_mandarin_to_ipa(word)
+    input = DocumentArray(Document(text=word, ipa=ipa))
+    return engine.search(input).to_dict()
+
+@app.get("/generate/")
+def generate(words: List[str]):
+    return generator.generate(words)

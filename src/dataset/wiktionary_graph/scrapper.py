69fe63e2f7e8cbf6d5b4f3e14ac0e67e0fb5f1d5
Simon Popelier <simon.popelier@gmail.com>
2022-12-12 23:25:25 +0100

    Edit wikitionaryparser in order to get links

--- src/dataset/wiktionary_graph/scrapper.py
+++ src/dataset/wiktionary_graph/scrapper.py
@@ -1,6 +1,11 @@
 from pathlib import Path
 from pydantic import BaseModel
 from typing import List
+from collections import deque
+from bs4 import BeautifulSoup
+import requests
+from os.path import join
+from src.dataset.wiktionary_graph.wiktionaryparser.core import WiktionaryParser
 
 class WiktionaryEntry(BaseModel):
     title: str # Used as label
@@ -13,10 +18,35 @@ class Scrapper():
     """
 
     def __init__(self):
-        self.url = Path('https://www.wiktionary.org/')
+        self.url = 'https://www.wiktionary.org/wiki/'
+        self.client = Neo4jClient(index_name="wiktionary")
+        self.parser = WiktionaryParser()
 
-    def scrap(self, word: str) -> WiktionaryEntry:
-        return WiktionaryEntry()
+    def breadth_first_search_scrapping(self, root: str, limit: int = None) -> List[WiktionaryEntry]:
+        already_visited = self.client.list_words()
+        queue = deque()
+        self.scrap(root)
+        while len(queue) > 0 and limit > 0:
+            word = queue.pop()
+            if not word in already_visited:
+                entry = self.scrap(word)
+                self.client.store(entry)
+                queue.extend(entry.related_words)
+                already_visited.append(word)
+                limit -=1
+
+    def scrap(self, word: str = "mnemonic") -> WiktionaryEntry:
+        results = self.parser.fetch(word)
+        links = []
+        related_words = []
+        for result in results:
+            for definition in result["definitions"]:
+                links.extend(definition['links'])
+                related_words.extend(definition['relatedWords'])
+            # result["pronunciations"] TODO pronunciations links
+            # TODO definition / s
+            # TODO: remove itself
+        return WiktionaryEntry(title=word, definition="", related_words=links)
 
     def store_entry(self, entry: WiktionaryEntry) -> None:
         """Stores entry into Neo4J database.
@@ -25,3 +55,18 @@ class Scrapper():
             entry (WiktionaryEntry): Entry to store.
         """
         pass
+
+class Neo4jClient():
+    def __init__(self, index_name: str):
+        self.index_name = index_name
+
+    def store(self, entry: WiktionaryEntry):
+        pass
+
+    def list_words(self) -> List[str]:
+        pass
+
+if __name__ == "__main__":
+    from pprint import pprint
+    scrapper = Scrapper()
+    pprint(scrapper.scrap())

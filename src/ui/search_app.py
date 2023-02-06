fa5c55b38f43f65661ce623d9f2c940d94f0c221
Simon Popelier <simon.popelier@gmail.com>
2023-02-06 21:53:24 +0100

    UI shows a clearer result (and fishes)

--- src/ui/search_app.py
+++ src/ui/search_app.py
@@ -1,10 +1,37 @@
 import streamlit as st
 import requests
 
-st.title("Pair Search App")
+title_alignment="""
+<style>
+h1 {
+  text-align: center
+}
+.stAlert {
+  text-align: center
+}
+</style>
+"""
+st.markdown(title_alignment, unsafe_allow_html=True)
 
-chinese_word = st.text_input("chinese_word")
+st.title(":tropical_fish: Finding Mnemo")
+
+chinese_word = st.text_input("Mandarin word:")
+
+def display_candidates(response):
+    matches = [match["text"] for x in response for match in x['matches'] ]
+    scores = [match["scores"]["cosine"]["value"] for x in response for match in x['matches'] ]
+    def score2emoji(score: float):
+        if score < 0.01:
+            return ":tropical_fish:"
+        elif score < 0.05:
+            return ":fish:"
+        else:
+            return ":blowfish:"
+    emojis = [score2emoji(x) for x in scores]
+    for index, (match, emoji) in enumerate(zip(matches, emojis)):
+        st.write(f"{emoji} - {match} ")
+    st.info(':tropical_fish: - Definitely Mnemo / :fish: - Not exactly Mnemo / :blowfish: - Barely Mnemo')
 
 if chinese_word:
-    response = requests.get(url=f"http://localhost:8000/search/{chinese_word}/")
-    st.write(response.json())
+    response = requests.get(url=f"http://localhost:8000/search/{chinese_word}/").json()
+    display_candidates(response)

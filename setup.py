5c1fbb96857fcd2dfec307ad5bf4d36bad2750ed
Simon Popelier <simon.popelier@gmail.com>
2023-05-28 21:29:56 +0200

    :package: Add more details in the setup file.

--- setup.py
+++ setup.py
@@ -1,17 +1,28 @@
 import os
+
 from setuptools import setup
+
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+
 setup(
-    name = "finding_mnemo",
-    version = "0.0.1",
-    author = "Simon Popelier",
-    author_email = "simon.popelier@gmail.com",
-    description = ("Finding mnemomnichs."),
-    license = "BSD",
-    keywords = "example documentation tutorial",
-    packages=['src'],
-    # package_dir={'':'mnemo'}
+    name="finding_mnemo",
+    version="0.0.1",
+    author="Simon Popelier",
+    author_email="simon.popelier@gmail.com",
+    description=("Finding mnemonics using different AI tools."),
+    long_description=read("README.md"),
+    classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Topic :: Text Processing :: Linguistic",
+    ],
+    license="BSD",
+    keywords=["mnemonic", "chinese", "mandarin", "english", "search", "phonetic"],
+    packages=["src"],
 )

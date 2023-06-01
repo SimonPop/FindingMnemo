69f0268c75887c2bbb44c0796b5cdebb603b149d
Simon Popelier <simon.popelier@gmail.com>
2023-06-02 00:24:07 +0200

    :coffin: Removed unused Jina code.

--- finding_mnemo/text_generation/generation/text_generator.py
+++ finding_mnemo/text_generation/generation/text_generator.py
@@ -1,10 +1,10 @@
 from typing import List
 
-from jina import DocumentArray, Executor, requests
+from docarray import DocumentArray
 from keytotext import pipeline
 
 
-class TextGenerator(Executor):
+class TextGenerator():
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.config = {
@@ -16,29 +16,9 @@ class TextGenerator(Executor):
         }
         self.model = pipeline("k2t-base")
 
-    @requests(on=["/generate"])
     def generate(self, docs: List[str], **kwargs):
         # for query in docs:
         #     mnemo = [self.model([query.text, keyword], **self.config) for keyword in query.matches[:,'text']]
         #     query.tags['mnemo'] = mnemo
         # return docs
         return self.model(docs, **self.config)
-
-
-from jina import Document, Flow
-
-if __name__ == "__main__":
-
-    inputs = DocumentArray(
-        [
-            Document(text="football", matches=[Document(text="Zidane")]),
-            Document(text="bakery", matches=[Document(text="bread")]),
-        ]
-    )
-
-    f = Flow().add(name="TextGenerator", uses=TextGenerator)
-    with f:
-        f.post(on="/generate", inputs=inputs, on_done=print)
-
-    # text_generator = TextGenerator()
-    # text_generator.generate(inputs)

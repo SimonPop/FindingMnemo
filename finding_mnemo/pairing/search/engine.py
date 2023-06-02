64dfa47747c8fd65fff9e0a036f949643301249c
Simon Popelier <simon.popelier@gmail.com>
2023-06-02 21:10:03 +0200

    :bug: Fix model config missing in Engine.

--- finding_mnemo/pairing/search/engine.py
+++ finding_mnemo/pairing/search/engine.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from typing import Dict, Union
 
+import json
 import torch
 from docarray import Document, DocumentArray
 
@@ -23,10 +24,24 @@ class Engine():
         return docs
 
     def load_model(self) -> PhoneticSiamese:
-        model = PhoneticSiamese()
+        with open(Path(__file__).parent.parent / "model" / "model_config.json", "r") as f:
+            model_config = json.load(f)
+        model = PhoneticSiamese(
+            embedding_dim=model_config["embedding_dim"],
+            dim_feedforward=model_config["dim_feedforward"],
+            nhead=model_config["nhead"],
+            dropout=model_config["dropout"],
+            batch_size=model_config["batch_size"],
+            weight_decay=model_config["weight_decay"],
+            lr=model_config["lr"],
+            margin=model_config["margin"],
+            lambda_triplet=model_config["lambda_triplet"],
+            lambda_pos=model_config["lambda_pos"],
+            lambda_neg=model_config["lambda_neg"],
+        )
         model.load_state_dict(
             torch.load(Path(__file__).parent.parent / "model" / "model_dict")
         )
-        model.eval()
         self.model = model
+        model.eval()
         return model
